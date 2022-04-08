fd200f42d41b4dbd86e0c49568448771c23cef01
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-09 02:58:18 +0600

    [ADD/DONE] firebase singin, signup, updateProfile etc working

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,12 @@
-import React, { useContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
 import "../firebase";
 
 const AuthContext = React.createContext();
@@ -11,12 +19,51 @@ export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
   const [currentUser, setCurrentUser] = useState(null);
 
+  useEffect(() => {
+    const auth = getAuth();
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unSubscribe;
+  }, []);
+
   //signup function
-  function signup() {
-    return;
+  async function signUp(email, password, username) {
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    //update profile
+    await updateProfile(auth.currentUser, { displayName: username });
+
+    //set current user
+    const user = auth.currentUser;
+    setCurrentUser({
+      ...user,
+    });
   }
+
+  //login function
+  async function logIn(email, password) {
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  //logout function
+  async function logOut() {
+    const auth = getAuth();
+    return signOut(auth);
+  }
+
+  const values = {
+    currentUser,
+    signUp,
+    logIn,
+    logOut,
+  };
+
   return (
-    <AuthContext.Provider value={AuthContext}>
+    <AuthContext.Provider value={values}>
       {!loading && children}
     </AuthContext.Provider>
   );
