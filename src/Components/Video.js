c448567679e63679ae683d134f0186e40eef478a
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-10 03:28:30 +0600

    [ADD] working for login system

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,18 +1,15 @@
-import { Link } from "react-router-dom";
 import thumb from "../assets/react.jpeg";
 import classes from "../styles/Video.module.css";
 
 export default function Video() {
   return (
-    <Link to="/quiz">
-      <div className={classes.video}>
-        <img src={thumb} alt="Video Title" />
-        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-        <div className={classes.qmeta}>
-          <p>10 Questions</p>
-          <p>Score : Not taken yet</p>
-        </div>
+    <div className={classes.video}>
+      <img src={thumb} alt="Video Title" />
+      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+      <div className={classes.qmeta}>
+        <p>10 Questions</p>
+        <p>Score : Not taken yet</p>
       </div>
-    </Link>
+    </div>
   );
 }
