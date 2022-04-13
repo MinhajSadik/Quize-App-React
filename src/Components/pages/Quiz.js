eccb8c1e62a8d3cfa711681e13afdb1f3815cd67
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-13 20:45:16 +0600

    [ADD] working for quiz answer and question uploading

--- src/Components/pages/Quiz.js
+++ src/Components/pages/Quiz.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+import { useParams } from "react-router-dom";
+import useQuestions from "../../Hooks/useQuestions";
 import Answers from "../Answers";
 import MiniPlayer from "../MiniPlayer";
 import ProgressBar from "../ProgressBar";
 
+const initialState = null;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "QUESTION":
+      return action.payload.forEach((question) => {
+        return question;
+      });
+  }
+};
+
 export default function Quiz() {
+  const { id } = useParams();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const { question, loading, error } = useQuestions(id);
+
+  const [qna, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       <h1>Pick three of your favorite start wars films</h1>
---
+++ src/Hooks/useQuestions.js
@@ -0,0 +1,55 @@
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from "firebase/database";
+import { useEffect, useState } from "react";
+
+export default function useQuestions(videoID) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [question, setQuestion] = useState([]);
+
+  useEffect(() => {
+    async function fetchQuestion() {
+      // database related works
+      const db = getDatabase();
+      const quizRef = ref(db, "quiz/" + videoID + "/questions");
+      const quizQuery = query(
+        quizRef,
+        orderByKey(),
+        startAt("" + 1),
+        limitToFirst(8)
+      );
+
+      try {
+        setError(false);
+        setLoading(true);
+        // request firebase database
+        const snapshot = await get(quizQuery);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setQuestion((prevQuestions) => {
+            return [...prevQuestions, ...Object.values(snapshot.val())];
+          });
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
+    }
+
+    fetchQuestion();
+  }, [videoID]);
+
+  return {
+    loading,
+    error,
+    question,
+  };
+}
