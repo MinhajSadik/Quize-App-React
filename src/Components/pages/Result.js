225fe03e1286a619e75119447acf01377c6d05d7
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-16 01:13:49 +0600

    [ADD/DONE] result, answer, score calculating worked complete

--- src/Components/pages/Result.js
+++ src/Components/pages/Result.js
@@ -1,7 +1,8 @@
+import _ from "lodash";
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
+import useAnswers from "../../Hooks/useAnswers";
 import Analysis from "../Analysis";
-import useAnswers from "../Hooks/useAnswers";
 import Summary from "../Summary";
 
 export default function Result() {
@@ -10,14 +11,36 @@ export default function Result() {
   const { state } = location;
   const { qna } = state;
   const { loading, error, answers } = useAnswers(id);
+  function calculate() {
+    let score = 0;
+    answers.forEach((question, index1) => {
+      let correctIndexes = [];
+      let chekcedIndexes = [];
+      question.options.forEach((option, index2) => {
+        if (option.correct) {
+          correctIndexes.push(index2);
+        }
+        if (qna[index1].options[index2].checked) {
+          chekcedIndexes.push(index2);
+          option.checked = true;
+        }
+      });
+      if (_.isEqual(correctIndexes, chekcedIndexes)) {
+        score += 5;
+      }
+    });
+    return score;
+  }
+
+  const userScore = calculate();
   return (
     <>
       {loading && <div>Loading...</div>}
       {error && <div>Error!</div>}
       {answers && answers.length > 0 && (
         <>
-          <Summary />
-          <Analysis />
+          <Summary score={userScore} noq={answers.length} />
+          <Analysis answers={answers} />
         </>
       )}
     </>
