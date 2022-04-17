8fff3cd28ebabd838872049ff1a76dba9e740236
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-18 02:55:03 +0600

    [ADD] miniPlayer worked done

--- src/Components/MiniPlayer.js
+++ src/Components/MiniPlayer.js
@@ -1,19 +1,48 @@
-import thumb from "../assets/react.jpeg";
+import { useRef, useState } from "react";
+import ReactPlayer from "react-player";
 import classes from "../styles/MiniPlayer.module.css";
 
-export default function MiniPlayer() {
+export default function MiniPlayer({ title, id }) {
+  const [status, setStatus] = useState(false);
+  const buttonRef = useRef();
+  const videoUrl = `https://www.youtube.com/watch?v=${id}`;
+
+  const toggleMiniPlayer = () => {
+    if (!status) {
+      buttonRef.current.classList.remove(classes.floatingBtn);
+      setStatus(true);
+    } else {
+      buttonRef.current.classList.add(classes.floatingBtn);
+      setStatus(false);
+    }
+  };
+
   return (
-    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
+    <div
+      className={`${classes.miniPlayer} ${classes.floatingBtn}`}
+      ref={buttonRef}
+      onClick={toggleMiniPlayer}
+    >
       <span className={`material-icons-outlined ${classes.open}`}>
         {" "}
         play_circle_filled{" "}
       </span>
-      <span className={`material-icons-outlined ${classes.close}`}>
+      <span
+        className={`material-icons-outlined ${classes.close}`}
+        onClick={toggleMiniPlayer}
+      >
         {" "}
         close{" "}
       </span>
-      <img src={thumb} alt="Alt Tag" />
-      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+      <ReactPlayer
+        // className={classes.player}
+        url={videoUrl}
+        width="300px"
+        height="168px"
+        playing={status}
+        controls
+      />
+      <p>{title}</p>
     </div>
   );
 }
